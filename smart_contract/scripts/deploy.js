30a0030b01529ed4c239836011bde632d41a98e2
huong666 <nguyenxuananhuong541@gmail.com>
2023-12-11 01:34:14 +0700

    add smart contract

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,31 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+const main = async () => {
+  const profileImageFactory = await hre.ethers.getContractFactory(
+    "ProfileImageNfts"
+  );
+  const profileImageContract = await profileImageFactory.deploy();
+
+  await profileImageContract.deployed();
+
+  console.log(
+    "Profile Image Minter deployed to:",
+    profileImageContract.address
+  );
+};
+
+(async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
