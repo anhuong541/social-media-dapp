991ca7c78e5c846ff633dd4ed039488deb81b561
huong666 <nguyenxuananhuong541@gmail.com>
2024-01-11 21:56:55 +0700

    fix error console.log

--- ui-client/src/pages/demo/index.tsx
+++ ui-client/src/pages/demo/index.tsx
@@ -24,7 +24,7 @@ export default function DemoPage() {
         console.error(`Error: ${response.data.message}`);
       }
     } catch (error) {
-      console.error(`Error fetching address information: ${error.message}`);
+      console.error(`Error fetching address information: ${error}`);
     }
   }
 
